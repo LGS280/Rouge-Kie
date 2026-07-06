bb24136dea4a2a3f4f8019b3c3189b18ce3c0be0
leebni <136786715+LGS280@users.noreply.github.com>
2026-07-06 11:45:27 +0700

    Revert "Fix mob synchronization, ID determinism, and room clearing issues"
    This reverts commit 4a9191a802f49a0e823f897fea43c65eba9264cc.

--- Assets/Script/Controller/MultiplayerSyncManager.cs
+++ Assets/Script/Controller/MultiplayerSyncManager.cs
@@ -97,15 +97,8 @@ public class MultiplayerSyncManager : MonoBehaviour
     private void AssignDeterministicRoomAndMobIds()
     {
         // 1. Đồng bộ Room ID
-        RoomController[] allRooms = Object.FindObjectsByType<RoomController>(FindObjectsInactive.Include, FindObjectsSortMode.None);
-        System.Array.Sort(allRooms, (a, b) => 
-        {
-            int cmpX = a.transform.position.x.CompareTo(b.transform.position.x);
-            if (cmpX != 0) return cmpX;
-            int cmpY = a.transform.position.y.CompareTo(b.transform.position.y);
-            if (cmpY != 0) return cmpY;
-            return string.CompareOrdinal(GetGameObjectPath(a.gameObject), GetGameObjectPath(b.gameObject));
-        });
+        RoomController[] allRooms = Object.FindObjectsByType<RoomController>(FindObjectsSortMode.None);
+        System.Array.Sort(allRooms, (a, b) => string.CompareOrdinal(GetGameObjectPath(a.gameObject), GetGameObjectPath(b.gameObject)));
 
         for (int i = 0; i < allRooms.Length; i++)
         {
@@ -113,15 +106,8 @@ public class MultiplayerSyncManager : MonoBehaviour
         }
 
         // 2. Đồng bộ Mob ID
-        MobNetworkIdentity[] allMobs = Object.FindObjectsByType<MobNetworkIdentity>(FindObjectsInactive.Include, FindObjectsSortMode.None);
-        System.Array.Sort(allMobs, (a, b) => 
-        {
-            int cmpX = a.transform.position.x.CompareTo(b.transform.position.x);
-            if (cmpX != 0) return cmpX;
-            int cmpY = a.transform.position.y.CompareTo(b.transform.position.y);
-            if (cmpY != 0) return cmpY;
-            return string.CompareOrdinal(GetGameObjectPath(a.gameObject), GetGameObjectPath(b.gameObject));
-        });
+        MobNetworkIdentity[] allMobs = Object.FindObjectsByType<MobNetworkIdentity>(FindObjectsSortMode.None);
+        System.Array.Sort(allMobs, (a, b) => string.CompareOrdinal(GetGameObjectPath(a.gameObject), GetGameObjectPath(b.gameObject)));
 
         for (int i = 0; i < allMobs.Length; i++)
         {
@@ -145,7 +131,7 @@ public class MultiplayerSyncManager : MonoBehaviour
 
     private void CacheAllRooms()
     {
-        RoomController[] allRooms = Object.FindObjectsByType<RoomController>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        RoomController[] allRooms = Object.FindObjectsByType<RoomController>(FindObjectsSortMode.None);
         foreach (RoomController room in allRooms)
         {
             if (string.IsNullOrEmpty(room.roomUniqueId))
@@ -362,7 +348,7 @@ public class MultiplayerSyncManager : MonoBehaviour
 
     private GameObject FindEnemyByNetworkId(string networkId)
     {
-        MobNetworkIdentity[] enemies = Object.FindObjectsByType<MobNetworkIdentity>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        MobNetworkIdentity[] enemies = Object.FindObjectsByType<MobNetworkIdentity>(FindObjectsSortMode.None);
         foreach (var enemy in enemies)
         {
             if (enemy.networkId == networkId)
